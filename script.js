bb83e005940b629984a1bedf540566a28c37b6d3
Syed Talha <syedt.in00@gmail.com>
2023-10-26 15:29:28 +0500

    final modifications completed

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 let input = document.querySelector(".container input");
 let checkBtn = document.querySelector(".container button");
-let infoTxt = document.querySelector(".info-tex");
+let infoTxt = document.querySelector(".info-text");
 let filterInput;
 
 checkBtn.addEventListener("click", () => {
     let reverseInput = filterInput.split("").reverse().join("");
-    infoTxt.style.display
-    if(reverseInput !== filterInput){
-        return console.log("Not a Palindrome");
-    };
-    console.log("Palindrome");
+    infoTxt.style.visibility = "visible";
+    if (reverseInput !== filterInput) {
+        infoTxt.innerHTML = `No, <span>'${input.value}'</span> isn't a Palindrome!`;
+    } else {
+        infoTxt.innerHTML = `Yes, <span>'${input.value}'</span> is a Palindrome!`;
+    }
 });
 
-input.addEventListener("keyup",() => {
-    filterInput = input.value.toLowerCase().replace(/[^A-Z0-9]/ig, "");
-    if(filterInput){
-        return checkBtn.classList.add("active");
+input.addEventListener("input", () => {
+    filterInput = input.value.toLowerCase().replace(/[^a-z0-9]/ig, "");
+    if (filterInput) {
+        checkBtn.classList.add("active");
+    } else {
+        infoTxt.style.visibility = "hidden";
+        checkBtn.classList.remove("active");
     }
-    return checkBtn.classList.remove("active");
 });
